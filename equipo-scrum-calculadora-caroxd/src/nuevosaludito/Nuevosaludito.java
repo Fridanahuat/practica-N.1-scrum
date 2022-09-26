6f1745f22bd7b65940b7e7c497aed2ea6d18832b
LicQuiterio <114037038+carolinaLagunaSV@users.noreply.github.com>
2022-09-25 20:30:21 -0500

    Modificación de mi saludo

---
+++ equipo-scrum-calculadora-caroxd/src/nuevosaludito/Nuevosaludito.java
@@ -0,0 +1,31 @@
+
+package nuevosaludito;
+
+import javax.swing.JOptionPane;
+
+public class Nuevosaludito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        JOptionPane.showMessageDialog(null, "Hola mi nombre es Carolina L :0");
+    }
+    
+}
