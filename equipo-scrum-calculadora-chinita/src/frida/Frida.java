e86c7822fe4d1e99ec8fcd8ee625540f45c90369
LicQuiterio <114036147+wilberthechazarreta@users.noreply.github.com>
2022-09-25 20:52:10 -0500

    MI SALUDOOOOOO

---
+++ equipo-scrum-calculadora-chinita/src/frida/Frida.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package frida;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fridanahuat
+ */
+public class Frida {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        JOptionPane.showMessageDialog(null, "Mi nombre es Wilberth Echazarreta");
+    }
+    
+}
